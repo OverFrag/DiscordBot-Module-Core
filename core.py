044b5c802aa80f363640cf6b9d669877d6595a89
Piotr Rzeczkowski <piotr@rzeka.net>
2016-03-22 15:54:47 +0100

    Adjust module to use directly in bot

--- core.py
+++ core.py
@@ -4,25 +4,37 @@ import datetime
 
 
 class Core(Module):
-    name = 'discordbot-core'
+    name = 'core'
+    uptime = None
+    msg = []
 
     def bot(self):
         self.config['init'] = datetime.datetime.now()
+
+        if 'core' in self.container.config['global']:
+            self.config = self.container.config['global']['core']
+
+        if 'website' in self.config:
+            self.msg.append(self.config['website'])
+
+        self.msg.append('Uptime: {}')
+
+        if 'description' in self.config and type(self.config['description']) is list:
+            self.msg.append('\n'.join(self.config['description']))
 
     async def on_message(self, message: discord.Message):
         if self.is_my(message):
             return False
 
         if self.has_command('info', message):
-            msg = [
-                self.config['info']['website'],
-                'Uptime ' + self.uptime(),
-                'Bot script: ' + self.config['info']['repository']
-            ]
+            self.uptime = self.__uptime()
 
-            await self.container.client.send_message(message.channel, '\n'.join(msg))
+            await self.container.client.send_message(
+                message.channel,
+                '\n'.join(self.msg).format(self.uptime)
+            )
 
-    def uptime(self):
+    def __uptime(self):
         def plural(val):
             if val == 1:
                 return ''
